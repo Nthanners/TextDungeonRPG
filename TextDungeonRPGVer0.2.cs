3aaea1fbeb4f674ad8bec04b0a1ae23909bcaa5c
Nthanners <130081631+Nthanners@users.noreply.github.com>
2023-06-18 10:42:19 +0800

    TextDungeonRPGVer1.cs

--- TextDungeonRPGVer0.2.cs
+++ TextDungeonRPGVer0.2.cs
@@ -2,12 +2,14 @@ using System;
 
 class Enemy
 {
+    public string Name { get; private set; }
     public int Health { get; private set; }
     public int Attack { get; private set; }
     public int Gold { get; private set; }
 
-    public Enemy(int health, int attack, int gold)
+    public Enemy(string name, int health, int attack, int gold)
     {
+        Name = name;
         Health = health;
         Attack = attack;
         Gold = gold;
@@ -25,14 +27,44 @@ class Enemy
         get { return Health > 0; }
     }
 
-    public static Enemy GenerateRandomEnemy()
+    public static Enemy GenerateRandomEnemy(int playerLevel)
     {
         Random random = new Random();
-        int health = random.Next(30, 50);
-        int attack = random.Next(5, 10);
-        int gold = random.Next(5, 10);
+        int health;
+        int attack;
+        int gold;
 
-        return new Enemy(health, attack, gold);
+        if (playerLevel % 5 == 0)
+        {
+            health = random.Next(40, 80);
+            attack = random.Next(9, 18);
+            gold = random.Next(17, 26);
+
+            int bossMultiplier = playerLevel / 5;
+            for (int i = 0; i < bossMultiplier; i++)
+            {
+                health *= 2;
+                attack *= 2;
+                gold *= 2;
+            }
+
+            return new Enemy("Boss", health, attack, gold);
+        }
+        else
+        {
+            health = random.Next(20, 35);
+            attack = random.Next(4, 9);
+            gold = random.Next(5, 10);
+
+            int levelMultiplier = playerLevel / 5;
+            health = (int)(health * Math.Pow(1.5, levelMultiplier));
+            attack = (int)(attack * Math.Pow(1.5, levelMultiplier));
+            gold = (int)(gold * Math.Pow(1.7, levelMultiplier));
+
+            string[] names = { "Goblin", "Skeleton", "Zombie", "Slime" };
+            string name = names[random.Next(names.Length)];
+            return new Enemy(name, health, attack, gold);
+        }
     }
 }
 
@@ -64,9 +96,17 @@ class Player
         Attack += 5;
         MaxHealth += 20;
         Gold -= levelUpCost;
-
-        Console.WriteLine("Congratulations! You've leveled up to level {0}.", PlayerLevel);
-        Console.WriteLine("Your health and attack have increased.");
+        
+        if (PlayerLevel % 5 == 0)
+        {
+            Console.WriteLine("Boss level! Brace yourself for a tough fight!");
+            Console.WriteLine("After the Boss is defeated the enemy is stronger.");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations! You've leveled up to level {0}.", PlayerLevel);
+            Console.WriteLine("Your health and attack have increased.");
+        }
         }
         else
         {
@@ -88,14 +128,84 @@ class Player
 
     public void Rest()
     {
+        int restCost = 5;
         int maxHealAmount = PlayerLevel * 100; 
         int missingHealth = MaxHealth - Health;
         int healAmount = Math.Min(maxHealAmount, missingHealth);
     
-        Health = Math.Min(MaxHealth, Health + healAmount); 
+     if (Gold >= restCost)
+        {
+            Health = Math.Min(MaxHealth, Health + healAmount);
+            Gold -= restCost;
 
-        Console.WriteLine("You are healed for {0} health.", healAmount);
-        Console.WriteLine("Your current health: {0}", Health);
+            Health = Math.Min(MaxHealth, Health + healAmount); 
+
+            Console.WriteLine("You are healed for {0} health.", healAmount);
+            Console.WriteLine("Your current health: {0}", Health);
+        }
+        else
+        {
+            Console.WriteLine("Insufficient gold to rest.");
+        }
+    }
+
+    public void BuyItem(string item, List<string> inventory)
+    {
+        switch (item)
+        {
+            case "sword":
+                BuySword(inventory);
+                break;
+            
+            case "armor":
+                BuyArmor(inventory);
+                break;
+
+            default:
+                Console.WriteLine("Invalid item!");
+                break;
+        }
+    }
+
+    private void BuySword(List<string> inventory)
+    {
+        int swordCost = 70;
+        if (Gold >= swordCost && !inventory.Contains("Sword"))
+        {
+            Gold -= swordCost;
+            Attack += 5;
+            inventory.Add("Sword");
+            Console.WriteLine("You have bought a sword! Your attack increased by 5.");
+        }
+        else if (inventory.Contains("Sword"))
+        {
+            Console.WriteLine("You already have a sword.");
+        }
+        else
+        {
+            Console.WriteLine("You don't have enough gold to buy a sword.");
+        }
+    }
+
+     private void BuyArmor(List<string> inventory)
+    {
+        int armorCost = 80;
+        if (Gold >= armorCost && !inventory.Contains("Armor"))
+        {
+            Gold -= armorCost;
+            Health += 100;
+            MaxHealth += 100;
+            inventory.Add("Armor");
+            Console.WriteLine("You have bought armor! Your health increased by 100.");
+        }
+        else if (inventory.Contains("Armor"))
+        {
+            Console.WriteLine("You already have armor.");
+        }
+        else
+        {
+            Console.WriteLine("You don't have enough gold to buy armor.");
+        }
     }
 }
 
@@ -104,6 +214,7 @@ class Program
     static void Main(string[] args)
     {
         Player player = new Player(100, 10, 0);
+        List<string> inventory = new List<string>();
 
         while (true)
         {
@@ -111,14 +222,18 @@ class Program
             Console.WriteLine("Player Health: {0}", player.Health);
             Console.WriteLine("Player Attack: {0}", player.Attack);
             Console.WriteLine("Player Gold: {0}", player.Gold);
+            Console.WriteLine("Inventory: {0}", string.Join(", ", inventory));
             Console.WriteLine();
 
             Console.WriteLine("MENU:");
             Console.WriteLine("1. Fight");
-            Console.WriteLine("2. Rest");
-            Console.WriteLine("3. Level Up(50 Gold)");
-            Console.WriteLine("4. Exit");
-            Console.WriteLine("Enter your choice (1-3):");
+            Console.WriteLine("2. Heal (5 Gold)");
+            Console.WriteLine("3. Level Up (50 Gold)");
+            Console.WriteLine("4. Shop");
+            Console.WriteLine("5. Code");
+            Console.WriteLine("6. Exit");
+            Console.WriteLine("Enter your choice (1-6):");
+
 
             int choice = int.Parse(Console.ReadLine());
             Console.WriteLine();
@@ -127,10 +242,10 @@ class Program
             {
                 case 1:
                     Console.WriteLine("You enter a dark room...");
-                    Console.WriteLine("A monster appears!");
-
-                    Enemy enemy = Enemy.GenerateRandomEnemy();
+                    Enemy enemy = Enemy.GenerateRandomEnemy(player.PlayerLevel);
                     bool fled = false;
+
+                    Console.WriteLine("A {0} appears!", enemy.Name);
 
                     while (enemy.IsAlive && player.IsAlive)
                     {
@@ -171,13 +286,6 @@ class Program
                                 }
                                 break;
 
-                                                                if (!player.IsAlive)
-                                {
-                                    Console.WriteLine("You were defeated! Game over.");
-                                    return;
-                                }
-                                break;
-
                             case 2:
                                 Console.WriteLine("You fled from the battle!");
                                 fled = true;
@@ -196,17 +304,66 @@ class Program
                     break;
 
                 case 2:
-                    Console.WriteLine("You find a safe spot to rest and regain your health.");
-                    player.Rest();
-                    Console.WriteLine("You feel refreshed and your health is fully restored.");
+                    if (player.Gold >= 5)
+                    {
+                        Console.WriteLine("You find a safe spot to rest and regain your health.");
+                        player.Rest();
+                        Console.WriteLine("You feel refreshed and your health is fully restored.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You don't have enough gold to rest.");
+                    }
                     break;
 
                 case 3:
                     player.LevelUp();
                     Console.WriteLine();
                     break;
-
+                
                 case 4:
+                    Console.WriteLine("Welcome to the Shop!");
+                    Console.WriteLine("What would you like to buy?");
+                    Console.WriteLine("1. Sword (+5 Attack): 70 Gold");
+                    Console.WriteLine("2. Armor (+100 Health): 80 Gold");
+                    Console.WriteLine("3. Back to Main Menu");
+                    Console.WriteLine("Enter your choice (1-3):");
+                    int shopChoice = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    switch (shopChoice)
+                    {
+                        case 1:
+                            player.BuyItem("sword", inventory);
+                            break;
+                        case 2:
+                            player.BuyItem("armor", inventory);
+                            break;
+                        case 3:
+                            Console.WriteLine("Returning to the menu...");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid item choice!");
+                            break;
+                    }
+                    break;
+
+                case 5:
+                    Console.WriteLine("Enter the secret code:");
+                    string secretCode = Console.ReadLine();
+
+                    if (secretCode == "Kronii")
+                    {
+                        player.Gold += 5000;
+                        Console.WriteLine("5000 gold has been added to your inventory!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid secret code!");
+                    }
+                    break;
+
+                case 6:
                     Console.WriteLine("Exiting game...");
                     return;
 
